68e4be4907ad1e85e5d2307707340253380d3ae1
FS <f.scholze@gmx.de>
2026-07-09 10:15:23 +0200

    Remove register and forgot password stuff

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,12 +1,5 @@
-import { Request, Response, NextFunction } from 'express'
-import { isUserAdmin } from '../helper/auth'
-import {
-  createKeycloakUser,
-  getKeycloakUsers,
-  loginWithPassword,
-  refreshUserToken,
-  sendPasswordResetEmail,
-} from '../helper/keycloak'
+import { Request, Response } from 'express'
+import { loginWithPassword, refreshUserToken } from '../helper/keycloak'
 
 export const login = async (req: Request, res: Response) => {
   const { email, password } = req.body
@@ -28,53 +21,6 @@ export const login = async (req: Request, res: Response) => {
     }
     return res.status(500).json({ message: (error as Error).message })
   }
-}
-
-export const register = async (req: Request, res: Response) => {
-  const { firstname, lastname, email, password } = req.body
-
-  if (!firstname || !lastname) {
-    return res.status(400).json({
-      message: 'First name and last name are required.',
-    })
-  }
-  if (!email) {
-    return res.status(400).json({ message: 'Email is required.' })
-  }
-  if (!password) {
-    return res.status(400).json({ message: 'Password is required.' })
-  }
-
-  try {
-    await createKeycloakUser({ firstname, lastname, email, password })
-    return res.json({ message: 'Registration successful' })
-  } catch (error) {
-    if ((error as Error).message === 'EMAIL_ALREADY_IN_USE') {
-      return res
-        .status(409)
-        .json({ message: 'This email address is already in use.' })
-    }
-    return res.status(500).json({ message: (error as Error).message })
-  }
-}
-
-export const forgotPassword = async (req: Request, res: Response) => {
-  const { email } = req.body
-
-  if (!email) {
-    return res.status(400).json({ message: 'Email is required.' })
-  }
-
-  try {
-    await sendPasswordResetEmail(email)
-  } catch {
-    // Always return success to avoid email enumeration
-  }
-
-  return res.json({
-    message:
-      'If an account with this email exists, a password reset link has been sent.',
-  })
 }
 
 export const refreshToken = async (req: Request, res: Response) => {
@@ -108,28 +54,5 @@ export const loginFree = async (req: Request, res: Response) => {
     return res.status(200).send()
   } catch (error) {
     res.status(400).json({ message: (error as Error).message })
-  }
-}
-
-export const IsUserAdmin = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  const { authorization } = req.headers
-  if (!authorization) return res.status(403).json({ message: 'Invalid token' })
-
-  const isAdmin = await isUserAdmin(authorization as string)
-  if (isAdmin) return next()
-
-  return res.status(403).json({ message: 'Forbidden' })
-}
-
-export const listKeycloakUsers = async (_req: Request, res: Response) => {
-  try {
-    const users = await getKeycloakUsers()
-    res.json(users)
-  } catch (error) {
-    res.status(500).json({ message: (error as Error).message })
   }
 }
